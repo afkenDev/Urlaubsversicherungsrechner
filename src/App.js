34cb3bd0889b70d240018580608c89ae008194ed
afkenDev <kenzo.abdelkebir@gmail.com>
2024-05-17 14:58:09 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div class="App">
+  <header class="App-header">
+    <h1 class="header logo">Urlaubsversicherungsrechner</h1>
+    <div class="steps">
+
     </div>
+    <div class="frage">Wollen sie sich im Ausland sicher fühlen?</div>
+    <button class="weiter">Start ⭢</button>
+  </header>
+</div>
+
   );
 }
 
