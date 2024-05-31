c9207ca99c8ffccd40ea587bd29f703d3b00b83d
afkenDev <kenzo.abdelkebir@gmail.com>
2024-05-31 14:56:58 +0200

    letzter commit von 31.05

--- src/App.js
+++ src/App.js
@@ -11,22 +11,35 @@ function App() {
   };
 
   const handleAnimationEnd = () => {
-    // was soll dann passieren
+    
   };
+
+  function increment(inputId) {
+    var input = document.getElementById(inputId);
+    input.value = parseInt(input.value) + 1;
+  }
+
+  function decrement(inputId) {
+    var input = document.getElementById(inputId);
+    if (parseInt(input.value) > 0) {
+      input.value = parseInt(input.value) - 1;
+    }
+  }
 
   return (
     <div className="App">
-      <div className="App-header">
-        <h1 className="header logo">Urlaubsversicherungsrechner</h1>
-        <div className="steps"></div>
-        <div className="frage">
-          Wollen Sie sich im Ausland sicher fühlen?
-        </div>
-        <Button onClick={handleButtonClick}>Start ⭢</Button>
-        {animatePlane && <PlaneImage onAnimationEnd={handleAnimationEnd} />}
+    <div className="App-header">
+      <h1 className="header logo">Urlaubsversicherungsrechner</h1>
+      <div className="steps"></div>
+      <div className="frage">
+        Wollen Sie sich im Ausland sicher fühlen?
       </div>
+      <Button onClick={handleButtonClick}>Start ⭢</Button>
+      {animatePlane && <PlaneImage onAnimationEnd={handleAnimationEnd} />}
     </div>
-  );
+  </div>
+  
+  );  
 }
 
 export default App;
